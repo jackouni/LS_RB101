b093addb3896a39af02f1dfe467cf46a8d408f15
Jack Sebben <jacksebben11@hotmail.com>
2023-12-26 18:16:29 -0500

    9th assignment, Ruby Version Managers. A short lesson. Here's the recap in an .rb file.

---
+++ lesson_2/09_ruby-version-managers.rb
@@ -0,0 +1,13 @@
+# Ruby Version Managers
+
+=begin 
+
+  It makes switching between different versions of Ruby in projects easier without 
+  having to re-configure file paths and download different versions for each project.
+
+  ## Some Ruby Version Managers include:
+    * rbnev
+    * chruby
+    * rvm
+    * asdf
+=end
