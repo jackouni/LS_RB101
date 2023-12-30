18826af0191ead63d5674ece3d6f0cfe7a88aecf
Jack Sebben <jacksebben11@hotmail.com>
2023-12-30 12:34:38 -0500

    Completed the second feature. Using the same validation method I used with the  method, I made a  method that validates a string input passed to it and uses the  to do so, similar to . Read last commit for understanding of how this works.

--- lesson_2/15_calculator-bonus.rb
+++ lesson_2/15_calculator-bonus.rb
@@ -2,8 +2,16 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end 
 
-def valid_number?(number)
-  number == number.to_i.to_s ? true : false
+def integer?(num)
+  num == num.to_i.to_s ? true : false
+end 
+
+def float?(num)
+  num == num.to_f.to_s ? true : false
+end
+
+def number?(num)
+  ( float?(num) || integer?(num) ) ? true : false
 end 
 
 def operation_to_message(op)
@@ -42,7 +50,7 @@ loop do # main loop
     prompt("What's your first number?")
     first_number = Kernel.gets().chomp()
 
-    if valid_number?(first_number)
+    if integer?(first_number)
       break
     else
       prompt("Please enter a valid number")
@@ -55,7 +63,7 @@ loop do # main loop
     prompt("What's your second number?")
     second_number = Kernel.gets().chomp()
 
-    if valid_number?(second_number)
+    if integer?(second_number)
       break
     else
       prompt("Please enter a valid number")
@@ -95,3 +103,39 @@ loop do # main loop
 end 
 
 prompt("Thank you for using this calculator. Goodbye.")
+
+# FEATURES TO ADD:
+
+=begin 
+1. Better integer validation. ✅
+
+  The current method of validating the input for a number is very weak. 
+  It's also not fully accurate, as you cannot enter a 0. 
+  Come up with a better way of validating input for integers.
+
+2. Number validation.
+
+  Suppose we're building a scientific calculator, 
+  and we now need to account for inputs that include decimals. 
+  How can we build a validating method, called `number?``,
+  to verify that only valid numbers -- integers or floats -- are entered?
+
+3. Our operation_to_message method is a little dangerous,
+  since we're relying on the case statement being the last expression in the method.
+  Suppose we needed to add some code after the case statement within the method? 
+  What changes would be needed to keep the method working with the rest of the program?
+
+4. Extracting messages in the program to a configuration file.
+
+  There are lots of messages sprinkled throughout the program. 
+  Could we move them into some configuration file and access by key? 
+  This would allow us to manage the messages much easier, and we could even internationalize the messages.
+
+5. Your calculator program is a hit, and it's being used all over the world! 
+  Problem is, not everyone speaks English. You need to now internationalize the messages in your calculator. 
+  You've already done the hard work of extracting all the messages to a configuration file. 
+  Now, all you have to do is send that configuration file to translators and call the right translation in your code.
+
+=end
+
+# ✅ = Complete
