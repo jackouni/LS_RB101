a9a33b9f7bcb498a8ad7299d6de98df5b2b1d531
Jack Sebben <jacksebben11@hotmail.com>
2024-01-01 11:58:04 -0700

    Loan/Mortgage calculator assignment is finished with some simple input validation and better UX and edge case handling. Again, this is very simple, assignment is complete though. Now to move onto the next assignment and potentially revisit this in the future.

--- lesson_2/16_Mortgage-calculator.rb
+++ lesson_2/16_Mortgage-calculator.rb
@@ -34,26 +34,73 @@ def apr_to_monthly_interest_rate(apr)
 end 
 
 puts "Welcome to the loan calculator!"
-puts "Enter your loan amount :"
-user_loan_amount = gets.chomp
 
-# valid_number?(user_loan)
+loop do # MAIN LOOP
 
-puts "Enter your loans duration (in years) :"
-user_loan_duration = gets.chomp
+  user_loan_amount = nil
 
-# valid_number?(user_loan_duration)
+  loop do 
+    puts "Enter your loan amount :"
+    user_loan_amount = gets.chomp
 
-puts "Enter your APR (annual interest rate) :"
-user_apr = gets.chomp
+    if valid_number?(user_loan_amount)
+      user_loan_amount.to_f.round(2)
+      break
+    else
+      puts "Please input a valid number!"
+    end 
+  end 
 
-# valid_number?(user_interest_rate)
+  user_loan_duration = nil
 
-user_monthly_interest = (user_apr.to_f / 12) / 100 # (In decimal format)
-user_month_duration = user_loan_duration.to_f * 12
+  loop do 
+    puts "Enter your loans duration (in years) :"
+    user_loan_duration = gets.chomp
 
-monthly_payments = user_loan_amount.to_f * (user_monthly_interest / (1 - (1 + user_monthly_interest)**(-user_month_duration)))
+    if valid_number?(user_loan_duration)
+      user_loan_duration.to_f.round(2)
+      break
+    else
+      puts "Please input a valid number!"
+    end     
+  end 
 
-puts "• Total of #{user_month_duration.floor} payments"
-puts "• Monthly Payments of $#{monthly_payments.round(2)}"
-puts "• Monthly Interest rate is #{(user_monthly_interest * 100).round(2)}%"
+  user_apr = nil 
+
+  loop do
+    puts "Enter your APR (annual percentage rate) :"
+    user_apr = gets.chomp
+
+    if valid_number?(user_apr)
+      user_apr.to_f.round(2)
+      break
+    else
+      puts "Please input a valid number!"
+    end 
+  end 
+    # valid_number?(user_interest_rate)
+
+  user_monthly_interest = (user_apr.to_f / 12) / 100 # (In decimal format)
+  user_month_duration = user_loan_duration.to_f * 12
+
+  monthly_payments = user_loan_amount.to_f * (user_monthly_interest / (1 - (1 + user_monthly_interest)**(-user_month_duration)))
+
+  puts "• Total of #{user_month_duration.floor} payments"
+  puts "• Monthly Payments of $#{monthly_payments.round(2)}"
+  puts "• Monthly Interest rate is #{(user_monthly_interest * 100).round(2)}%"
+
+  user_input = nil
+
+  loop do
+    puts "Would you like to perform another caluclation?\nType 'y' to continue or 'n' to exit/quit."
+    user_input = gets.chomp.downcase
+
+    if user_input == 'y' || user_input == 'n'
+      break
+    else
+      puts "Please input a valid option!\nMust be 'y' (continue) or 'n' (exit/quit)."
+    end
+  end 
+
+  break if user_input == 'n'
+end
