5f1e5d0e0ab459bb72fc687bde407714e5711bbc
Jack Sebben <jacksebben11@hotmail.com>
2024-01-02 09:23:07 -0700

    Added a  method that formats our programs prompts to have a '=>' preceding the message, this way we can better segregate a prompt from the user's inputs, making for a better UX.  added after each prompting section to keep things seperated and less cluttered in the terminal. Better input validation in the  method, now it checks for empty strings and values less than 0 too. Some other housekeeping to clean up the UI as well. Now it's finished (tentativley).

--- lesson_2/16_Mortgage-calculator.rb
+++ lesson_2/16_Mortgage-calculator.rb
@@ -14,15 +14,20 @@
 
 =end
 
-def valid_number?(input)
-  if input == input.to_f.to_s 
-    return true
-  elsif input == input.to_i.to_s 
+def valid_number?(input) 
+  if input.to_i < 0 || input.to_f < 0 || input.empty?
+    return false
+  elsif input == input.to_f.to_s || input == input.to_i.to_s
     return true
   else 
     return false
   end 
 end 
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
 
 def get_monthly_payment(loan_amount, monthly_interest_rate, month_duration)
   monthly_payment = loan_amount * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-month_duration)))
@@ -33,48 +38,51 @@ def apr_to_monthly_interest_rate(apr)
  return (apr.to_i / 12)
 end 
 
-puts "Welcome to the loan calculator!"
+prompt("Welcome to the loan calculator!")
 
 loop do # MAIN LOOP
   user_loan_amount = nil
 
   loop do 
-    puts "Enter your loan amount :"
+    prompt("Enter your loan amount :")
     user_loan_amount = gets.chomp
 
     if valid_number?(user_loan_amount)
       user_loan_amount.to_f.round(2)
+      puts " "
       break
     else
-      puts "Please input a valid number!"
+      prompt("Please input a valid number!")
     end 
   end 
 
   user_loan_duration = nil
 
   loop do 
-    puts "Enter your loans duration (in years) :"
+    prompt("Enter your loans duration (in years) :")
     user_loan_duration = gets.chomp
 
     if valid_number?(user_loan_duration)
       user_loan_duration.to_f.round(2)
+      puts " "
       break
     else
-      puts "Please input a valid number!"
+      prompt("Please input a valid number!")
     end     
   end 
 
   user_apr = nil 
 
   loop do
-    puts "Enter your APR (annual percentage rate) :"
+    prompt("Enter your APR (annual percentage rate) :")
     user_apr = gets.chomp
 
     if valid_number?(user_apr)
       user_apr.to_f.round(2)
+      puts " "
       break
     else
-      puts "Please input a valid number!"
+      prompt("Please input a valid number!")
     end 
   end 
 
@@ -83,22 +91,29 @@ loop do # MAIN LOOP
 
   monthly_payments = user_loan_amount.to_f * (user_monthly_interest / (1 - (1 + user_monthly_interest)**(-user_month_duration)))
 
-  puts "• Total of #{user_month_duration.floor} payments"
-  puts "• Monthly Payments of $#{monthly_payments.round(2)}"
-  puts "• Monthly Interest rate is #{(user_monthly_interest * 100).round(2)}%"
+  prompt("Loan Stats:")
+  prompt("• Total of #{user_month_duration.floor} payments")
+  prompt("• Monthly Payments of $#{monthly_payments.round(2)}")
+  prompt("• Monthly Interest rate is #{(user_monthly_interest * 100).round(2)}%")
+  puts " "
 
   user_input = nil
 
   loop do
-    puts "Would you like to perform another caluclation?\nType 'y' to continue or 'n' to exit/quit."
+    prompt("Would you like to perform another caluclation?\nType 'y' to continue or 'n' to exit/quit.")
     user_input = gets.chomp.downcase
 
     if user_input == 'y' || user_input == 'n'
+      puts " " 
       break
     else
-      puts "Please input a valid option!\nMust be 'y' (continue) or 'n' (exit/quit)."
+      prompt("Please input a valid option!\nMust be 'y' (continue) or 'n' (exit/quit).")
     end
   end 
 
-  break if user_input == 'n'
+  if user_input == 'n'
+    prompt("Thank you for using our calculator.")
+    prompt("Goodbye!")
+    break
+  end 
 end
