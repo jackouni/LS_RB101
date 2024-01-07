a32deb352e44f09fdac61216ed0fa2e4fc78320d
Jack Sebben <jacksebben11@hotmail.com>
2024-01-06 20:29:28 -0500

    Fixed almost all of rubocop's offenses. The code does look a lot cleaner and is less error prone, so that's good. The code is easier to read too. The only offense that remains is the naming of the actual file itself, not maintaining the snake case naming convention. I agree with this offense and think for the sake of consistency I will keep the snake case + kebab naming, but for the next lesson just have snake case.

--- lesson_2/22_rock-paper-scissors-rubocop.rb
+++ lesson_2/22_rock-paper-scissors-rubocop.rb
@@ -2,19 +2,15 @@ Kernel.puts("Welcome to LS's 'Rock. Paper, Scissors'!")
 
 VALID_CHOICES = %w(rock paper scissors)
 
+WIN_COMBOS = [['rock', 'scissors'], ['scissors', 'paper'], ['paper', 'rock']]
+
 def prompt(message)
   Kernel.puts "=> #{message}"
 end
 
 def win?(first, second)
-  if (first == 'rock' && second == 'scissors')  ||
-    (first == 'scissors' && second == 'paper') ||
-    (first == 'paper' && second == 'rock')
-    true 
-  else
-    false
-  end 
-end 
+  WIN_COMBOS.include?([first, second])
+end
 
 def display_results(player, computer)
   if win?(player, computer)
@@ -23,8 +19,8 @@ def display_results(player, computer)
     prompt("You Lost!")
   else
     prompt("It's a tie.")
-  end 
-end 
+  end
+end
 
 loop do
   choice = ''
@@ -35,13 +31,13 @@ loop do
 
     if VALID_CHOICES.include?(choice)
       break
-    else 
+    else
       prompt("That's not a valid choice.")
-    end 
-  end 
+    end
+  end
 
   computer_choice = VALID_CHOICES.sample()
-  
+
   prompt("You chose: #{choice}; Computer chose: #{computer_choice};")
 
   display_results(choice, computer_choice)
