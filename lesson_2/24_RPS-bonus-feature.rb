77529067f36cedb76c7ac89d87f63a8c73d8a1b4
Jack Sebben <jacksebben11@hotmail.com>
2024-01-07 21:59:58 -0500

    Assignment 24 file created, although I will be using a Git branch on assignment 22 file to make changes and then merge it to that file. What will happen to this file, I don't know. I most likely will delete it shortly.

---
+++ lesson_2/24_RPS-bonus-feature.rb
@@ -0,0 +1,14 @@
+=begin 
+  1) Lizard Spock
+    This game is a variation on the Rock Paper Scissors game that adds two more options - Lizard and Spock. The full explanation and rules are here. There's also a hilarious Big Bang Theory video about it here.
+
+    The goal of this bonus is to add Lizard and Spock into your code.
+
+  2) Typing the full word "rock" or "lizard" is tiring. Update the program so the user can type "r" for "rock," "p" for "paper," etc. Note that if you do bonus #1, you'll have two words that start with "s." How do you resolve that?
+
+  3) Keep score of the player's and computer's wins. 
+    When either the player or computer reaches three wins, 
+    the match is over, and the winning player becomes the grand winner. 
+    Don't add your incrementing logic to display_results. 
+    Keep your methods simple; they should perform one logical task — no more, no less. 
+=end
