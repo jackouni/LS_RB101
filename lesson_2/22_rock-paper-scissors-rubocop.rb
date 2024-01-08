bfc693f225236c73d53572183525189b1929519c
Jack Sebben <jacksebben11@hotmail.com>
2024-01-07 23:10:10 -0500

    Game now keeps track of the score and displays the score to the user. This is done using the  method to check to see who won a round - if player wins then the  variable is incremented by 1, elsif computer wins then the  variable is incremented by 1. Following is the  method that takes the  and  variables and displays their values to the console.

--- lesson_2/22_rock-paper-scissors-rubocop.rb
+++ lesson_2/22_rock-paper-scissors-rubocop.rb
@@ -28,9 +28,16 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
+def display_scores(player_score, computer_score)
+  prompt("Player Score: #{player_score}; Computer Score: #{computer_score};")
+end 
 
+player_score = 0 
+computer_score = 0
+
+loop do
+  
+  choice = ''
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}\n( You can also type: #{ALIASES.join(', ')}. )")
     choice = Kernel.gets().chomp()
@@ -47,9 +54,15 @@ loop do
 
   computer_choice = VALID_CHOICES.sample()
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice};")
+  if win?(choice, computer_choice)
+    player_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end 
 
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice};")
   display_results(choice, computer_choice)
+  display_scores(player_score, computer_score)
 
   prompt("Do you want to play again?")
   answer = Kernel.gets().chomp()
