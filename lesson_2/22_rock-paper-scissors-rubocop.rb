a2c5a9028c8df8c5ad65f696c511577f2818c965
Jack Sebben <jacksebben11@hotmail.com>
2024-01-07 23:29:10 -0500

    Reformatted the WIN_COMBOS array to have it's nested arrays to be inline vertically, as it is more readable. Removed trailing whitespaces and also moved the prompt text on line 56 to take up a new line, making it more readable.

--- lesson_2/22_rock-paper-scissors-rubocop.rb
+++ lesson_2/22_rock-paper-scissors-rubocop.rb
@@ -3,7 +3,16 @@ Kernel.puts("Welcome to LS's 'Rock. Paper, Scissors, Lizard, Spock'!")
 ALIASES = %w(r p sc l sp)
 VALID_CHOICES = %w(rock paper scissors lizard spock)
 
-WIN_COMBOS = [['rock', 'scissors'], ['scissors', 'paper'], ['paper', 'rock'], ['rock', 'lizard'], ['lizard', 'spock'], ['spock', 'scissors'], ['scissors', 'lizard'], ['lizard', 'paper'], ['paper', 'spock'], ['spock', 'rock']]
+WIN_COMBOS = [['rock', 'scissors'],
+              ['scissors', 'paper'],
+              ['paper', 'rock'],
+              ['rock', 'lizard'],
+              ['lizard', 'spock'],
+              ['spock', 'scissors'],
+              ['scissors', 'lizard'],
+              ['lizard', 'paper'],
+              ['paper', 'spock'],
+              ['spock', 'rock']]
 
 def prompt(message)
   Kernel.puts "=> #{message}"
@@ -16,7 +25,7 @@ def get_winner(player_choice, computer_choice)
     "player"
   else
     "computer"
-  end 
+  end
 end
 
 def alias_to_full_word(choice)
@@ -36,16 +45,16 @@ end
 
 def display_scores(player_score, computer_score)
   prompt("Player Score: #{player_score}; Computer Score: #{computer_score};")
-end 
+end
 
-player_score = 0 
+player_score = 0
 computer_score = 0
 
 loop do
-  
   player_choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}\n( You can also type: #{ALIASES.join(', ')}. )")
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}
+    You can also type: #{ALIASES.join(', ')}.")
     player_choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(player_choice)
